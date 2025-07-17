d2e8bdbdc3b7097fffef73074c922593f2c6100d
OjedaAndres <ojedajoaquinandres@gmail.com>
2025-07-17 15:20:25 -0400

    Add function edit

--- main.js
+++ main.js
@@ -1,14 +1,27 @@
 const formulario = document.querySelector('.from-create');
+let cont = 1;
 formulario.addEventListener('submit', (e)=>{
     e.preventDefault();
     const descripcion = document.querySelector('#descripcion').value;
     const contenedor = document.querySelector('.publicaciones');
     contenedor.innerHTML += `
         <div class="publicacion">
-            <p>id: 00</p>
+            <p>id:${cont}</p>
             <p>${descripcion}</p>
-            <button>editar</button>
-            <button>Eliminar</button>
+            <button onclick="editar(this)">editar</button>
+            <button onclick="eliminar(this)">Eliminar</button>
         </div>`;
-
+    cont++;
 });
+
+function editar(e){
+    const parrafo =e.parentElement.querySelector('p:nth-child(2)');
+    parrafo.contentEditable = true;
+    parrafo.focus();
+    parrafo.onblur = () => parrafo.contentEditable = false;
+    console.log(parrafo);
+} 
+
+function eliminar(e){
+    e.parentElement.remove();
+}
